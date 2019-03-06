969ba201af364de4d0da8d82417761270e7aeeda
valiullinadinara <dianadura>
2019-03-06 22:01:11 +0300

    1
    1

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$( "#img" ).on("mouseenter", function() {
+  $( "#img" ).animate({
+    left: "+=1050",
+    top: "+=350"
+
+  }, 2000, function() {
+  });
+
+  $( "#img2" ).animate({
+    // left: "+=1050",
+    top: "+=350"
+
+  }, 2000, function() {
+  });
+});
+
+$( "#img2" ).on("mouseenter", function() {
+  $( "#img2" ).animate({
+
+    left: "+=1050",
+    top: "+=350"
+
+  }, 2000, function() {
+  });
+});
+
+// $( "#img2" ).on("mouseenter", function() {
+//   $( "#img2" ).animate({
+//
+//     left: "+=1050",
+//     top: "+=350"
+//
+//
+//
+//
+//   }, 2000, function() {
+//   });
+// });
